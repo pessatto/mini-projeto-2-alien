ca6164cedc518091faafed3daff7126289a406eb
eduardorezmatos <eduardorezende987@gmail.com>
2026-09-25 15:51:49 -0300

    função 4 feita

--- main.c
+++ main.c
@@ -23,6 +23,25 @@ void inverter(char *s)
     }
 }
 
+//Função 4
+void inverterCaixa(char *s)
+{
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        if (65 <= s[i] && s[i] <= 90)
+        {
+            s[i]+= 32;
+        }
+        else if(97 <= s[i] && s[i] <= 122)
+        {
+            s[i] -=32;
+        }
+        i++;
+    }
+}
+
+
 int main()
 {
     int Operacao;
