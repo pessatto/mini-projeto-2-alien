bfd772f3f83b1fe6fc62f4ceccf8024abe2baa76
João Pedro Pessatto <joaopedro.pessatto@gmail.com>
2026-09-25 17:01:15 -0300

    Função 2 implementada

--- main.c
+++ main.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
-
-//Função Auxiliar Tamanho da String
-
-int strlen (char *s)
+// Função Auxiliar Tamanho da String
+int strlen(char *s)
 {
     int tam = 0, j = 0;
-    while(s[j] != '\0')
+    while (s[j] != '\0')
     {
-        tam ++;
+        tam++;
         j++;
     }
     return tam;
 }
-
 
 // Função 1
 void inverter(char *s)
@@ -38,7 +35,60 @@ void inverter(char *s)
     }
 }
 
-//Função 4
+//Função 2
+void deslocar(char *s, int n)
+{
+    int i, valor_passou, valor_coube;
+    int n_letra = n % 26;
+    int n_digito = n % 10;
+    for (i = 0; *(s + i) != '\0'; i++)
+    {
+        if (*(s + i) >= 'a' && *(s + i) <= 'z')
+        {
+            if ((*(s + i) + n_letra) > 'z')
+            {
+                valor_passou = (*(s + i) + n_letra) - 'z';
+                valor_coube = n_letra - valor_passou;
+                *(s + i) += valor_coube;
+                *(s + i) = ('a' - 1) + valor_passou;
+            }
+            else
+            {
+                *(s + i) += n_letra;
+            }
+        }
+        else if (*(s + i) >= 'A' && *(s + i) <= 'Z')
+        {
+            if ((*(s + i) + n_letra) > 'Z')
+            {
+                valor_passou = (*(s + i) + n_letra) - 'Z';
+                valor_coube = n_letra - valor_passou;
+                *(s + i) += valor_coube;
+                *(s + i) = ('A' - 1) + valor_passou;
+            }
+            else
+            {
+                *(s + i) += n_letra;
+            }
+        }
+        else if (*(s + i) >= '0' && *(s + i) <= '9')
+        {
+            if ((*(s + i) + n_digito) > '9')
+            {
+                valor_passou = (*(s + i) + n_digito) - '9';
+                valor_coube = n_digito - valor_passou;
+                *(s + i) += valor_coube;
+                *(s + i) = ('0' - 1) + valor_passou;
+            }
+            else
+            {
+                *(s + i) += n_digito;
+            }
+        }
+    }
+}
+
+// Função 4
 void inverterCaixa(char *s)
 {
     int i = 0;
@@ -46,44 +96,41 @@ void inverterCaixa(char *s)
     {
         if (65 <= s[i] && s[i] <= 90)
         {
-            s[i]+= 32;
+            s[i] += 32;
         }
-        else if(97 <= s[i] && s[i] <= 122)
+        else if (97 <= s[i] && s[i] <= 122)
         {
-            s[i] -=32;
+            s[i] -= 32;
         }
         i++;
     }
 }
 
-//Função 6
-void trocarMetades (char *s)
+// Função 6
+void trocarMetades(char *s)
 {
     int i;
     char temp;
     int tam = strlen(s);
     int metade = tam / 2;
     metade = (tam % 2 == 0) ? metade : metade + 1;
-    for(i = 0; i < metade - 1; i++)
+    for (i = 0; i < metade - 1; i++)
     {
         temp = s[i];
         s[i] = s[i + metade];
         s[i + metade] = temp;
     }
-
 }
-
 
 int main()
 {
-    int Operacao, tamanho_string = 0, j = 0;
+    int Operacao, tamanho_string = 0, j = 0, deslocar_qntd;
     char string[10000];
 
     scanf("%[^\n]", string);
     scanf("%d", &Operacao);
-    
 
-    while(Operacao != 0)
+    while (Operacao != 0)
     {
         switch (Operacao)
         {
@@ -92,13 +139,14 @@ int main()
             break;
 
         case 2:
-            /* Deslocar Caracter */
+            scanf("%d", &deslocar_qntd);
+            deslocar(string, deslocar_qntd);
             break;
 
         case 3:
             /* Trocar Pares */
             break;
-            
+
         case 4:
             inverterCaixa(string);
             break;
@@ -106,21 +154,19 @@ int main()
         case 5:
             /* Rotacionar msg */
             break;
-            
+
         case 6:
             trocarMetades(string);
             break;
-        
+
         default:
             break;
         }
         scanf("%d", &Operacao);
     }
 
-    //imprimir resultado
+    // imprimir resultado
     printf("%s\n", string);
-
-
 
     return 0;
 }
