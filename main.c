b72d8b90263ad2c997ad1349886365b43ea82e25
eduardorezmatos <eduardorezende987@gmail.com>
2026-09-25 15:27:14 -0300

    Implement operation selection in main function
    Added a switch statement to handle various operations based on user input.

--- main.c
+++ main.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
 
+
 int main()
 {
+    int Operacao;
+
+    scanf("%d", &Operacao);
+
+    while(Operacao != 0)
+    {
+        switch (Operacao)
+        {
+        case 1:
+            /* Inverter Mensagem */
+            break;
+
+        case 2:
+            /* Deslocar Caracter */
+            break;
+
+        case 3:
+            /* Trocar Pares */
+            break;
+            
+        case 4:
+            /* Inverter Maiusc/Minusc */
+            break;
+
+        case 5:
+            /* Rotacionar msg */
+            break;
+            
+        case 6:
+            /* Trocar Metades */
+            break;
+        
+        default:
+            break;
+        }
+        scanf("%d", &Operacao);
+    }
+
+    //imprimir resultado
+
 
     return 0;
 }
