8074c6e68f209ca372ca9785fc722853fe540457
João Pedro Pessatto <joaopedro.pessatto@gmail.com>
2026-09-25 15:38:23 -0300

    Primeira função feita.

--- main.c
+++ main.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 
+// Função 1
+void inverter(char *s)
+{
+    int i = 0, j = 0;
+    char temp;
+
+    while (*(s + j) != '\0')
+    {
+        j++;
+    }
+    j--;
+
+    while (i < j)
+    {
+        temp = *(s + i);
+        *(s + i) = *(s + j);
+        *(s + j) = temp;
+
+        i++;
+        j--;
+    }
+}
 
 int main()
 {
     int Operacao;
+    char string[10000];
 
+    scanf("%[^\n]", string);
     scanf("%d", &Operacao);
+    
 
     while(Operacao != 0)
     {
         switch (Operacao)
         {
         case 1:
-            /* Inverter Mensagem */
+            inverter(string);
             break;
 
         case 2:
@@ -42,6 +67,7 @@ int main()
     }
 
     //imprimir resultado
+    printf("%s\n", string);
 
 
     return 0;
